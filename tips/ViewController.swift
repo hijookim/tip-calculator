0611d7451ec244f621a5876a422d82ea31524962
Karen Kim <karen.kim@airbnb.com>
2016-03-12 19:28:46 -0800

    [tip-calc] Implement all optional features

--- tips/ViewController.swift
+++ tips/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var billField: UITextField!
     @IBOutlet weak var tipLabel: UILabel!
     @IBOutlet weak var totalLabel: UILabel!
     
+    @IBOutlet weak var amountView: UIView!
+    @IBOutlet weak var splitView: UIView!
+    @IBOutlet weak var multiSplitInfoView: UIView!
+    
+    @IBOutlet weak var split2PeopleAmountLabel: UILabel!
+    @IBOutlet weak var split3PeopleAmountLabel: UILabel!
+    @IBOutlet weak var split4PeopleAmountLabel: UILabel!
+    
+    var tipPercentSegControlValues = [0.18, 0.20, 0.22]
+    var defaultTipPercent = 0.0
+    var totalSplitViewVisible = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
+  
+        initializeTipFieldLabels()
+        self.amountView.alpha = 1
+        self.multiSplitInfoView.alpha = 0
+        billField.becomeFirstResponder()
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let defaultTipPercentageValue = defaults.integerForKey("default_tip_percentage")
+        defaultTipPercent = Double(defaultTipPercentageValue) / 100
+        
+        setTipLabels()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        // store current state to remember for next load
+        let defaults = NSUserDefaults.standardUserDefaults()
+        defaults.setObject(billField.text!, forKey: "bill_amount")
+        defaults.setObject(NSDate(), forKey: "last_used_datetime")
+        defaults.synchronize()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,23 +61,106 @@ class ViewController: UIViewController {
     }
 
     @IBAction func onEditingChanged(sender: AnyObject) {
-        var tipPercentages = [0.18, 0.20, 0.22]
-        let tipPercentage = tipPercentages[tipControl.selectedSegmentIndex]
-    
-        let billAmount = (billField.text! as NSString).doubleValue
-        let tipAmount = billAmount * tipPercentage
-        let totalAmount = billAmount + tipAmount
-        
-        tipLabel.text = "$\(tipAmount)"
-        totalLabel.text = "$\(totalAmount)"
-        
-        tipLabel.text = String(format: "$%.2f", tipAmount)
-        totalLabel.text = String(format: "$%.2f", totalAmount)
-
+        setTipLabels()
     }
 
     @IBAction func onTap(sender: AnyObject) {
         view.endEditing(true)
+        toggleTotalSplitView()
+    }
+    
+    @IBAction func onTipPercentSegControlTap(sender: AnyObject) {
+        let selectedIndex = (sender as! UISegmentedControl).selectedSegmentIndex
+        defaultTipPercent = tipPercentSegControlValues[selectedIndex]
+        setTipLabels()
+    }
+    
+    func toggleTotalSplitView() {
+        if totalSplitViewVisible {
+           // hide the split view
+            billField.becomeFirstResponder()
+            UIView.animateWithDuration(0.4, animations: {
+                let amountViewTopFrame = self.amountView.frame
+                var splitViewTopFrame = self.splitView.frame
+                splitViewTopFrame.origin.y += amountViewTopFrame.size.height
+                self.splitView.frame = splitViewTopFrame
+                
+                self.amountView.alpha = 1
+                self.multiSplitInfoView.alpha = 0
+            })
+            
+            totalSplitViewVisible = false
+        } else {
+            // make split view visible
+            view.endEditing(true)
+            
+            UIView.animateWithDuration(0.4, animations: {
+                let amountViewTopFrame = self.amountView.frame
+                var splitViewTopFrame = self.splitView.frame
+                splitViewTopFrame.origin.y -= amountViewTopFrame.size.height
+                self.splitView.frame = splitViewTopFrame
+                
+                self.amountView.alpha = 0
+                self.multiSplitInfoView.alpha = 1
+            })
+            
+            totalSplitViewVisible = true
+        }
+    }
+    
+    // used to set the last used bill amount if within 10 min of last used time. if not, set to default value
+    func initializeTipFieldLabels() {
+        var loadedLastBillAmount = false
+        let defaults = NSUserDefaults.standardUserDefaults()
+        let lastUsedDateTime = defaults.objectForKey("last_used_datetime")
+        
+        if let lastUsedDate = lastUsedDateTime as! NSDate? {
+            // compute expiration date by adding 10 min
+            let minutesToAddInSeconds: NSTimeInterval = 10 * 60
+            let expirationDate = lastUsedDate.dateByAddingTimeInterval(minutesToAddInSeconds)
+            
+            // if not past the expiration time, then load previous bill amount
+            if expirationDate > NSDate() {
+                let latestBillAmount = defaults.stringForKey("bill_amount")
+                billField.text = latestBillAmount
+                setTipLabels()
+                loadedLastBillAmount = true
+            }
+        }
+        
+        if !loadedLastBillAmount {
+            tipLabel.text = formatPrice(0.0)
+            totalLabel.text = formatPrice(0.0)
+        }
+    }
+    
+    func formatPrice(price: Double) -> String {
+        let formatter = NSNumberFormatter()
+        formatter.numberStyle = .CurrencyStyle
+        return formatter.stringFromNumber(price)!
+    }
+    
+    func setTipLabels() {
+        let billAmount = Double(billField.text!)!
+        let tipAmount = billAmount * defaultTipPercent
+        let totalAmount = billAmount + tipAmount
+        
+        tipLabel.text = formatPrice(tipAmount)
+        totalLabel.text = formatPrice(totalAmount)
+        split2PeopleAmountLabel.text = formatPrice(totalAmount / 2)
+        split3PeopleAmountLabel.text = formatPrice(totalAmount / 3)
+        split4PeopleAmountLabel.text = formatPrice(totalAmount / 4)
     }
 }
+
+// NSDate comparator extensions
+public func ==(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs === rhs || lhs.compare(rhs) == .OrderedSame
+}
+
+public func <(lhs: NSDate, rhs: NSDate) -> Bool {
+    return lhs.compare(rhs) == .OrderedAscending
+}
+
+extension NSDate: Comparable { }
 
